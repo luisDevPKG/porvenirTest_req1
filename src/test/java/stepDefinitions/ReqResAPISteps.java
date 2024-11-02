171cf335df792d16c4ecdc35cba56e13c34a6afb
Luis F Mosquera <luisfmosquera97@gmail.com>
2024-11-02 13:29:40 -0500

    Fix: Se arregla el steps y el freature para la correcta ejecucion del primer scenario

--- src/test/java/stepDefinitions/ReqResAPISteps.java
+++ src/test/java/stepDefinitions/ReqResAPISteps.java
@@ -17,17 +17,24 @@ public class ReqResAPISteps {
     /* scenarios */
     @Given("that I access the ReqRes user listing service")
     public void that_I_access_the_ReqRes_user_listing_service() {
-        String URL = "https://reqres.in/api";
+        String URL = "https://reqres.in";
         RestAssured.baseURI = URL;
         request = RestAssured.given();
     }
-    @When("I make a GET request to the endpoint")
-    public void I_make_a_GET_request_to_the_endpoint() {
-        response = request.get("/users?page=2");
+    @When("I make a GET request to the endpoint {string}")
+    public void I_make_a_GET_request_to_the_endpoint(String path) {
+        response = request.get(path);
     }
-    @Then("Then the response should contain the list of users on page and the status of the response should be")
-    public void Then_the_response_should_contain_the_list_of_users_on_page_and_the_status_of_the_response_should_be() {
-        response.then().statusCode(200);
-        response.then().body("page", equalTo(2));
+    @Then("The response should contain the list of users on the page {int} and the response status should be {int}")
+    public void Then_the_response_should_contain_the_list_of_users_on_page_and_the_status_of_the_response_should_be(int page, int status) {
+        // Verificar que la respuesta no es nula
+        Assert.assertNotNull("The response is null. Make sure the request was successful.", response);
+        // Verificar el código de estado de la respuesta
+        response.then().statusCode(status);
+
+        // Extraer y verificar el valor de "page" en la respuesta
+        Integer actualPage = response.jsonPath().getInt("page");
+        Assert.assertNotNull("The 'page' key was not found in the response.", actualPage);
+        Assert.assertEquals("The 'page' value did not match the expected value.", page, actualPage.intValue());
     }
 }
--- src/test/resources/features/ReqResAPI.feature
+++ src/test/resources/features/ReqResAPI.feature
@@ -2,5 +2,5 @@ Feature: ReqRes API automatedTesting
 
   Scenario: Users list
     Given that I access the ReqRes user listing service
-    When I make a GET request to the endpoint
-    Then Then the response should contain the list of users on page and the status of the response should be
+    When I make a GET request to the endpoint "/api/users?page=2"
+    Then The response should contain the list of users on the page 2 and the response status should be 200
